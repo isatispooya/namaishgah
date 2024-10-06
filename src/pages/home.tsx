dd8bc170a2959106b0cbc6c492edb6a67765bb92
ZahraAmini <112477210+ZahraAmini1112@users.noreply.github.com>
2024-10-06 10:29:41 +0330

    .

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import GreetingsSection from "../modules/greetings/features/banner";
+import QaPage from "../modules/user/pages/Qa.page";
+
+const Home = ()=> {
+<div className="container">
+    <div className="row">
+        <GreetingsSection/>
+        <QaPage/>
+    </div>
+</div>
+}
+
+export default Home;
